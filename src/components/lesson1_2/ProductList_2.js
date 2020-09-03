5744d2763367b4fc7d46a9ade4ad877d1f9354ba
kharchenkok <k.v.kharchenko@ukr.net>
2020-09-03 14:06:59 +0300

    commit

--- src/components/lesson1_2/ProductList_2.js
+++ src/components/lesson1_2/ProductList_2.js
@@ -5,8 +5,13 @@ export function ProductList({products}){
     console.log('products', products);
     return (
         <ul style={{display:"flex", flexWrap: "wrap", listStyle:'none', justifyContent:'space-between'}}>
-            {products.map(product=> 
-            product.isFreeShipping ? (<ProductListItem key={product.id}{...product}/>): null)}
+            {products.map(product=><ProductListItem key={product.id}{...product}/>)}
         </ul>
     );
 }
+
+
+
+
+// {products.map(product=> 
+//     product.isFreeShipping ? (<ProductListItem key={product.id}{...product}/>): null)}
